d736cb95c76effe23554c64d5e8a0fd020efd2c7
Fadi Atamny <fadi@dataloop.ai>
2024-03-20 16:53:17 +0200

    main fixes

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <template>
     <DlThemeProvider :is-dark="isDark">
-        <img alt="Vue logo" src="./assets/dataloop-logo.svg" class="logo" />
-        <DlTypography variant="h1">
-            {{ buttonLabel }}
-            {{ buttonStatus }}
-            <div style="padding-left: 50%">
-                Last updated: {{ lastUpdated }}
-                <DlButton
-                    :label="buttonLabel"
-                    :disabled="buttonStatus"
-                    @click="onClick"
-                />
-                <DlProgressBar
-                    label="Progress bar"
-                    v-bind="{
-                        width: '200px',
-                        value: 0.5,
-                        showValue: true,
-                        showPercentage: true
-                    }"
-                />
+        <div v-if="!isReady" class="loading-spinner">
+            <DlSpinner text="Loading App..." size="128px" />
+        </div>
+        <div v-else>
+            <div class="top-bar">
+                <div
+                    style="
+                        display: flex;
+                        flex-direction: row;
+                        align-items: center;
+                        gap: 15px;
+                    "
+                >
+                    <DlTypography variant="h4">
+                        Last updated: {{ lastUpdated }}
+                    </DlTypography>
+                    <DlButton
+                        :label="buttonLabel"
+                        :disabled="buttonStatus"
+                        outlined
+                        @click="onClick"
+                    />
+                </div>
+                <div>
+                    <DlProgressBar
+                        label="Progress bar"
+                        v-bind="{
+                            width: '200px',
+                            value: 0.5,
+                            showValue: true,
+                            showPercentage: true
+                        }"
+                    />
+                </div>
             </div>
-        </DlTypography>
-        <div class="container">
-            <iframe
-                ref="contentIframe"
-                class="frame-container"
-                :src="iframeSource"
-            ></iframe>
+            <div class="container">
+                <iframe ref="contentIframe" class="frame-container"></iframe>
+            </div>
         </div>
     </DlThemeProvider>
 </template>
@@ -37,37 +47,54 @@ import {
     DlThemeProvider,
     DlTypography,
     DlButton,
-    DlProgressBar
+    DlProgressBar,
+    DlSpinner
 } from '@dataloop-ai/components'
 import { DlEvent } from '@dataloop-ai/jssdk'
 import { onMounted } from 'vue'
 import { ref } from 'vue-demi'
 
-const iframeSource = ref('src/loading.html')
-// let iframeSource = ref('http://localhost:3003/insights/main');
-const isDark = ref(true)
-const lastUpdated = ref('Never')
-const buttonStatus = ref(false)
-const buttonLabel = ref('Run')
-let dataset_id = null
-let project_id = null
+const contentIframe = ref<HTMLIFrameElement | null>(null)
+const isReady = ref<boolean>(false)
+const isDark = ref<boolean>(true)
+const lastUpdated = ref<string>('Never')
+const buttonStatus = ref<boolean>(false)
+const buttonLabel = ref<string>('Run')
+const datasetId = ref<string>(null)
+const projectId = ref<string>(null)
 
-async function main() {
-    console.log('in main')
-    lastUpdated.value = new Date().toLocaleTimeString()
-    buttonLabel.value = 'Fetching'
-    console.log(`${buttonLabel.value}`)
-    buttonStatus.value = true
-    console.log(`${buttonStatus.value}`)
-    project_id = (await window.dl.projects.get()).id
-    dataset_id = (await window.dl.datasets.get()).id
-    iframeSource.value = `http://localhost:3003/insights/build/${dataset_id}`
-    buttonLabel.value = 'Run'
-    buttonStatus.value = false
+const loadFrame = async (src: string) => {
+    contentIframe.value.onload = () => {
+        setTimeout(() => {
+            buttonLabel.value = 'Run'
+            buttonStatus.value = false
+        }, 5000)
+    }
+    contentIframe.value.src = `http://localhost:3003/insights/build/${datasetId.value}`
 }
 
-async function changeIframeTheme(theme) {
-    fetch(`/insights/settings/${dataset_id}`, {
+const triggerMainAppLoad = async () => {
+    lastUpdated.value = new Date().toLocaleTimeString()
+    buttonLabel.value = 'Fetching'
+    buttonStatus.value = true
+
+    const project = await window.dl.projects.get()
+    projectId.value = project?.id ?? null
+    const dataset = await window.dl.datasets.get()
+    datasetId.value = dataset?.id ?? null
+
+    if (datasetId.value) {
+        loadFrame(`http://localhost:3003/insights/build/${datasetId.value}`)
+    } else {
+        console.error('No dataset found to fetch insights')
+
+        buttonLabel.value = 'Run'
+        buttonStatus.value = false
+    }
+}
+
+const changeIframeTheme = async (theme) => {
+    fetch(`/insights/settings/${datasetId.value}`, {
         method: 'POST', // Specify the method
         headers: {
             'Content-Type': 'application/json' // Specify the content type
@@ -81,58 +108,54 @@ async function changeIframeTheme(theme) {
         .catch((error) => {
             console.error('Error:', error) // Handle errors
         })
-    iframeSource.value = `http://localhost:3003/insights/build/${dataset_id}`
+
+    loadFrame(`http://localhost:3003/insights/build/${datasetId.value}`)
 }
 
 onMounted(() => {
-    console.log(`${iframeSource.value}`)
-    try {
-        window.onload = function () {
-            console.log('on load function')
-            async function init() {
-                await window.dl.on('ready', async () => {
-                    console.log('In init')
-                    const settings = await window.dl.settings.get()
-                    changeIframeTheme(settings.theme)
-                    await window.dl.on('theme', (data) => {
-                        changeIframeTheme(data)
-                    })
-                    await main()
-                })
-            }
-            init()
-        }
-    } catch (e) {
-        console.error('Error initializing xFrameDriver', e)
-    }
+    window.dl.on(DlEvent.READY, async () => {
+        const settings = await window.dl.settings.get()
+        changeIframeTheme(settings.theme)
+        window.dl.on(DlEvent.THEME, (data) => {
+            changeIframeTheme(data)
+        })
+        await triggerMainAppLoad()
+        isReady.value = true
+    })
 })
 
 async function onClick() {
-    await main()
+    await triggerMainAppLoad()
 }
 </script>
 
 <style scoped>
 .container {
     display: flex;
-    /* Make the container a flexbox */
-    /* Stack elements vertically */
-    width: 100vw;
+    width: 100%;
     height: 100vh;
-    /* Set container height to 100% viewport height */
     justify-content: center;
 }
 
 .container > * {
-    /* Target all direct children of the container */
     flex: 0 0 auto;
-    /* Set default size for child elements */
 }
 
 .container iframe {
-    /* Style the iframe specifically */
     flex: 1 1 auto;
-    /* Allow iframe to expand to fill remaining space */
     width: 100%;
+}
+
+.loading-spinner {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+}
+
+.top-bar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px 0;
 }
 </style>
